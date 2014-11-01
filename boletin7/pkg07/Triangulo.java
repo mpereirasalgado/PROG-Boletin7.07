c7858521a6fac71d63d1ce242942937afc6647ca
Seijas <Seijas@192.168.0.101>
2014-11-01 17:51:59 +0100

    ctriangulo añadido introducion datos

--- boletin7/pkg07/Triangulo.java
+++ boletin7/pkg07/Triangulo.java
@@ -15,6 +15,18 @@ public class Triangulo {
         this.altura = altura;
     }
     
+    //introducion datos
+    public double setBase(){
+        String resposta = JOptionPane.showInputDialog(null, "Introduce la base del triangulo a calcular");
+        double valor = Double.valueOf(resposta);
+        return valor;
+    }
+    public double setAltura(){
+        String resposta = JOptionPane.showInputDialog(null, "Introduce la base del triangulo");
+        double valor = Double.valueOf(resposta);
+        return valor;
+    }
+    
     //calcular area
     public void calcularArea(double base, double altura){
         JOptionPane.showMessageDialog(null, "El area del triangulo de base " + base + " y altura " + altura + " es de " + ((base*altura)/2) + "m^2");
