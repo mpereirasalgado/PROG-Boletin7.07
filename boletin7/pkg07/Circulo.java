d49907b8f84569f61dfee0af0f1c1aec4842b34a
Seijas <Seijas@192.168.0.101>
2014-11-01 17:36:15 +0100

    circulo echo

--- boletin7/pkg07/Circulo.java
+++ boletin7/pkg07/Circulo.java
@@ -1,8 +1,24 @@
 //Write by Seijas
 package boletin7.pkg07;
 
+import javax.swing.JOptionPane;
+
 public class Circulo {
+    double PI = 3.14159;
+    double radio;
     
+    //Constructores
+    public Circulo(){
+        radio = 0;
+    }
+    public Circulo(double radio){
+        this.radio = radio;
+    }
+    
+    //calcular area
+    public void calcularArea(double radio){
+        JOptionPane.showMessageDialog(null, "El area del circulo de radio " + radio + " son " + (PI*radio) + "m^2");
+    }
 }
 //Created by the best programer (Seijas)
 //To my lovers <3
