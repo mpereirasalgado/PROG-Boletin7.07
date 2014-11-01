23e370ba39eb5fc53d4b02342bf7e8d48dfd82d4
Seijas <Seijas@192.168.0.101>
2014-11-01 18:44:37 +0100

    añadido contenido opciones, falta switch

--- boletin7/pkg07/Boletin707.java
+++ boletin7/pkg07/Boletin707.java
@@ -1,9 +1,31 @@
 //Write by Seijas
 package boletin7.pkg07;
 
+import javax.swing.JOptionPane;
+
 public class Boletin707 {
 
     public static void main(String[] args) {
+        double lado;
+        double base, altura;
+        double radio;
+
+        
+        //opcion duadrado
+        Cuadrado obx1 = new Cuadrado();
+        lado = obx1.setLado();
+        obx1.calcularArea(lado);
+        
+        //Opcion triangulo
+        Triangulo obx2 = new Triangulo();
+        base = obx2.setBase();
+        altura = obx2.setAltura();
+        obx2.calcularArea(base, altura);
+        
+        //opcion circulo
+        Circulo obx3 = new Circulo();
+        radio = obx3.setRadio();
+        obx3.calcularArea(radio);
         
     }
 }
