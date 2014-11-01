2c6e8123e68a9fc30124ce9fa188c161b9680577
Seijas <Seijas@192.168.0.101>
2014-11-01 17:45:23 +0100

    cuadrado echo

--- boletin7/pkg07/Cuadrado.java
+++ boletin7/pkg07/Cuadrado.java
@@ -1,8 +1,23 @@
 //Write by Seijas
 package boletin7.pkg07;
 
+import javax.swing.JOptionPane;
+
 public class Cuadrado {
+    double lado;
     
+    //constructores
+    public Cuadrado(){
+        lado = 0;
+    }
+    public Cuadrado(double lado){
+        this.lado = lado;
+    }
+    
+    //calcular area
+    public void calcularArea(double lado){
+        JOptionPane.showMessageDialog(null, "El area del cuadrado de lado " + lado + " es de " + (Math.pow(lado, 2)) + "m^2");
+    }
 }
 //Created by Seijas
 //To my Fans and lovers <3
